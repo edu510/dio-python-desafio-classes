7f4041323c1c441fdaa3b43eee2e907a8ceaae9d
edu510 <www.eduardo.f.n@gmail.com>
2024-10-21 01:45:43 -0300

    feat: Adiciona classes Cliente, PessoaFisica e Historico

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -98,6 +98,43 @@ class ContaCorrente(Conta):
             C/C:\t\t{self.numero}
             Titular:\t{self.cliente.nome}
         """
+
+
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
+
+class PessoaFisica(Cliente):
+    def __init__(self, cpf, nome, data_nascimento, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+
+
+class Historico:
+    def __init__(self):
+        self._transacoes = []
+
+    @property
+    def transacoes(self):
+        return self._transacoes
+
+    def adicionar_transacao(self, transacao):
+        self._transacoes.append(
+            {
+                "tipo": transacao.__class__.__name__,
+                "valor": transacao.valor,
+                "data": datetime.now().strftime("%d-%m-%Y %H:%M:%s"),
+            }
+        )
 def adiciona_operacao_com_tempo(dict, operacao):
     """Adiciona uma operacao ao dict, usando como chave o tempo atual"""
     tempo_agora = datetime.now()
