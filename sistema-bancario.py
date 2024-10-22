13924ffc65f525d24c345ba8e2400525aee86b8e
edu510 <www.eduardo.f.n@gmail.com>
2024-10-22 00:06:22 -0300

    chore: Remove metodos antigos que nao usam as classes

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -10,16 +10,8 @@ menu = """
 
 => """
 
-saldo = 0
-limite = 500
-numero_saques = 0
-proximo_num_conta = 1
-LIMITE_SAQUES = 3
-AGENCIA = "0001"
-extrato = {}
 
 usuarios = []
-contas = []
 
 
 class Conta:
@@ -235,63 +227,6 @@ class Deposito(Transacao):
             conta.historico.adicionar_transacao(self)
 
 
-def adiciona_operacao_com_tempo(dict, operacao):
-    """Adiciona uma operacao ao dict, usando como chave o tempo atual"""
-    tempo_agora = datetime.now()
-    if dict.__contains__(tempo_agora):
-        dict[tempo_agora].append(operacao)
-    else:
-        dict[tempo_agora] = [operacao]
-
-def exibir_extrato(dict):
-    for tempo, operacao in dict.items():
-        print(f"{tempo:%d-%m-%Y %H:%M:%S} - {operacao}")
-
-def sacar(*, valor, saldo, extrato, limite, numero_saques, limite_saques):
-    
-    excedeu_saldo = valor > saldo
-    excedeu_limite = valor > limite
-    excedeu_saques = numero_saques >= limite_saques
-
-    if excedeu_saldo:
-        print("Operação falhou! Você não tem saldo suficiente.")
-
-    elif excedeu_limite:
-        print("Operação falhou! O valor do saque excede o limite.")
-
-    elif excedeu_saques:
-        print("Operação falhou! Número máximo de saques excedido.")
-
-    elif valor <= 0:
-        print("Operação falhou! O valor informado é inválido.")
-
-    else:
-        saldo -= valor
-        adiciona_operacao_com_tempo(extrato, f"Saque: R$ {valor:.2f}")
-        numero_saques += 1
-        print(f"Saque de R${valor:.2f} realizado")
-        print(f"Saldo atualizado: R${saldo:.2f}")
-
-    return saldo
-
-def depositar(valor, saldo, extrato, /):
-    if valor > 0:
-        saldo += valor
-        adiciona_operacao_com_tempo(extrato, f"Depósito: R$ {valor:.2f}")
-        print(f"Deposito de R${valor:.2f} realizado")
-        print(f"Saldo atualizado: R${saldo:.2f}")
-
-    else:
-        print("Operação falhou! O valor informado é inválido.")
-
-    return saldo
-
-def gerar_extrato(saldo, /, *, extrato):
-    print("\n================ EXTRATO ================")
-    print("Não foram realizadas movimentações.") if not extrato else exibir_extrato(extrato)
-    print(f"\nSaldo: R$ {saldo:.2f}")
-    print("==========================================")
-
 def verifica_se_usuario_existe(usuarios, cpf):
     for pessoa in usuarios:
         if pessoa.cpf == cpf:
@@ -329,17 +264,6 @@ def encontrar_usuario_por_cpf(usuarios, cpf):
     print("Não há usuario com esse cpf")
     return None
 
-def criar_conta(contas, agencia, num_conta, usuarios):
-    """ Retorna o valor do proximo numero de conta """
-    cpf = int(input("Digite o cpf do usuario ao qual a conta sera vinculada: "))
-    usuario = encontrar_usuario_por_cpf(usuarios, cpf)
-    if(not usuario):
-        print("Nao existe usuario com esse CPF. Cancelando operacao.")
-        return num_conta
-    contas.append({"agencia":agencia, "num_conta":num_conta, "usuario":usuario})
-    print("Conta criada com sucesso!")
-    return num_conta + 1
-
 def listar_usuarios(usuarios):
     if not usuarios:
         print("Nenhum usuario foi criado ainda.")
@@ -349,19 +273,6 @@ def listar_usuarios(usuarios):
         print(usuario)
         print()
 
-def listar_contas(contas):
-    if not contas:
-        print("Nenhuma conta foi criada ainda.")
-        return
-    print()
-    for conta in contas:
-        agencia, num, usuario = conta.values()
-        cpf = usuario["cpf"]
-        print(f"conta {num}:")
-        print(f"agencia - {agencia}")
-        print(f"cpf do usuario - {cpf}\n")
-
-    
 def input_cpf_para_usuario():
     cpf = input("Digite o cpf do usuario:\t")
     return encontrar_usuario_por_cpf(usuarios, cpf)
